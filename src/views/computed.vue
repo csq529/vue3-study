b49044e78d14a831c06d8adfa3974c137e76be2e
csq529 <946081940@qq.com>
2022-01-19 10:38:14 +0800

    setup

--- src/views/computed.vue
+++ src/views/computed.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: cui
  * @Date: 2022-01-19 09:51:12
- * @LastEditTime: 2022-01-19 10:12:14
+ * @LastEditTime: 2022-01-19 10:23:52
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue3-jsj/src/views/computed.vue
@@ -12,23 +12,13 @@
   <div>{{ doubleNum }}</div>
 </template>
 
-<script>
+<script setup>
 import { ref, computed } from 'vue'
-export default {
-  setup() {
-    let num = ref(0)
-
-    function add () {
-      num.value = num.value + 1
-    }
-    const doubleNum = computed(() => num.value * 2)
-    return {
-      num,
-      doubleNum,
-      add
-    }
-  }
+let num = ref(0)
+function add () {
+  num.value = num.value + 1
 }
+const doubleNum = computed(() => num.value * 2)
 </script>
 
 <style>
